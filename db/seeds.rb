8bb9fc97e18e6b73be0d6cea512f29cd5aae388c
fernandojerez <fernandojerezy@gmail.com>
2023-06-27 12:50:21 -0400

    se modifica seed y se ejecuta y funciona correctamente

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Crear 15 películas aleatorias
+15.times do
+  Movie.create(
+    name: Faker::Movie.title,
+    synopsis: Faker::Lorem.paragraph,
+    director: Faker::Name.name
+  )
+end
+
+# Crear 15 series aleatorias
+15.times do
+  Series.create(
+    name: Faker::TvShows::Simpsons.character,
+    synopsis: Faker::Lorem.paragraph,
+    director: Faker::Name.name
+  )
+end
+
+# Crear 15 documentales aleatorios
+15.times do
+  DocumentaryFilm.create(
+    name: Faker::Lorem.word,
+    synopsis: Faker::Lorem.paragraph,
+    director: Faker::Name.name
+  )
+end
